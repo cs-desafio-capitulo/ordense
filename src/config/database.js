01f0194959f6ff61453df533f55d8adbec60c2ad
cs-matheus-galdino <matheus.galdino@hotmail.com>
2017-12-21 09:28:30 -0200

    Conexão com banco de dados MLab realizada

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+// FIXME: deveria puxar de arquivo de variáveis globais
+const databaseUrl = 'mongodb://cs-matheus-galdino:cs-desafio@ds161146.mlab.com:61146/cs-desafio';
+
+const connectToDatabase = () => mongoose.connect(databaseUrl, {
+  useMongoClient: true,
+}, connectionError => (connectionError) ? connectionError : '');
+
+export default { connectToDatabase };
