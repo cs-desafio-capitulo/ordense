367b6fcb57e1aebd8e4d4350ec8fd92b46b11910
Matheus Galdino <matheus.galdino@hotmail.com>
2018-01-05 14:08:54 -0200

    Updated database reference

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ import env from '../config/env';
 
 mongoose.Promise = global.Promise;
 
-const databaseUrl = env.db.url;
+const databaseUrl = env.db.url || 'mongodb://cs-matheus-galdino:cs-desafio@ds161146.mlab.com:61146/cs-desafio';
+
 
 const connectToDatabase = () => mongoose.connect(databaseUrl, {
   useMongoClient: true,
